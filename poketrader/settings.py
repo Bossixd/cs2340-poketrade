6555253a99eede0f263555e3f54f8aedafc6ef61
Bossixd <bosspattakit123@gmail.com>
2025-04-13 00:00:55 -0400

    rearrenge Installed Apps

--- poketrader/settings.py
+++ poketrader/settings.py
@@ -39,10 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "pokemon",
-    "auths",
     "landing",
     "pokehub",
-    "accounts"
+    "accounts",
+    "auths",
 ]
 
 MIDDLEWARE = [
